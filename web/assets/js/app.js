4e9e8c8f29f43d336e08b7e76b3cbb9b107a5424
Collin Van der Vorst <collin@littlemissrobot.com>
2018-01-24 17:14:10 +0100

    first styling

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -68,23 +68,20 @@
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(1);
-module.exports = __webpack_require__(4);
+module.exports = __webpack_require__(2);
 
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__) {
+/***/ (function(module, exports) {
 
-"use strict";
 
 
 /***/ }),
-/* 2 */,
-/* 3 */,
-/* 4 */
+/* 2 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: \n@import \"base/organisms/_hero\";\n^\n      File to import not found or unreadable: /Users/lmr/code/stefan-duiven/resources/sass/base/organisms/_hero.scss.\nParent style sheet: stdin\n      in /Users/lmr/code/stefan-duiven/resources/sass/base.scss (line 58, column 1)\n    at runLoaders (/Users/lmr/code/stefan-duiven/node_modules/webpack/lib/NormalModule.js:195:19)\n    at /Users/lmr/code/stefan-duiven/node_modules/loader-runner/lib/LoaderRunner.js:364:11\n    at /Users/lmr/code/stefan-duiven/node_modules/loader-runner/lib/LoaderRunner.js:230:18\n    at context.callback (/Users/lmr/code/stefan-duiven/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (/Users/lmr/code/stefan-duiven/node_modules/sass-loader/lib/loader.js:55:13)\n    at Object.<anonymous> (/Users/lmr/code/stefan-duiven/node_modules/sass-loader/node_modules/async/dist/async.js:2257:31)\n    at Object.callback (/Users/lmr/code/stefan-duiven/node_modules/sass-loader/node_modules/async/dist/async.js:958:16)\n    at options.error (/Users/lmr/code/stefan-duiven/node_modules/node-sass/lib/index.js:294:32)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
